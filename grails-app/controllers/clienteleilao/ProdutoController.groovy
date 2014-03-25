f9e13644c7797337961796a53d4aea0633d9405b
Eder Brendo <eder_brendo@hotmail.com>
2014-03-25 16:56:32 -0300

    Ajustes...

--- grails-app/controllers/clienteleilao/ProdutoController.groovy
+++ grails-app/controllers/clienteleilao/ProdutoController.groovy
@@ -9,126 +9,130 @@ import groovy.json.JsonSlurper
 @Transactional(readOnly = true)
 class ProdutoController {
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+	static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
-    def index(Integer max) {
-		
-	
-			def test = new URL("http://luizvarela666-public_sales.nodejitsu.com/products").text
-			def slurper = new JsonSlurper()
-			def result = slurper.parseText(test)
-			
-			request.setAttribute('result',result)
-			
-			flash.message = "some message"
-			
-			respond result
-			
-		
-		
-        /*params.max = Math.min(max ?: 10, 100)
-        respond Produto.list(params), model:[produtoInstanceCount: Produto.count()]*/
-    }
+	def index(Integer max) {
 
-    def show(Produto produtoInstance) {
-        respond produtoInstance
-    }
 
-    def create() {
-        respond new Produto(params)
-    }
+		def test = new URL("http://luizvarela666-public_sales.nodejitsu.com/products").text
+		def slurper = new JsonSlurper()
+		def result = slurper.parseText(test)
+
+		request.setAttribute('result',result)
+
+		respond result
+	}
+
+	def show(Produto produtoInstance) {
+		respond produtoInstance
+	}
+
+	def create() {
+		respond new Produto(params)
+	}
 
 	def submitForm() {
 		def urlBase = "http://luizvarela666-public_sales.nodejitsu.com"
 		def urlPath = "/products"
 		def postData = params
 		withHttp(uri: "http://www.luizvarela666-public_sales.nodejitsu.com/products") {
-		   def retorno = post(body:params)
-		   println retorno
-		  
+			def retorno = post(body:params)
+			println retorno
 		}
-		
+
 		flash.message = "Produto Cadastrado com Sucesso!"
-		
+
 		redirect(uri: "/")
 	}
-		
-    @Transactional
-    def save(Produto produtoInstance) {
-        if (produtoInstance == null) {
-            notFound()
-            return
-        }
 
-        if (produtoInstance.hasErrors()) {
-            respond produtoInstance.errors, view:'create'
-            return
-        }
+	@Transactional
+	def save(Produto produtoInstance) {
+		if (produtoInstance == null) {
+			notFound()
+			return
+		}
 
-        produtoInstance.save flush:true
+		if (produtoInstance.hasErrors()) {
+			respond produtoInstance.errors, view:'create'
+			return
+		}
 
-        request.withFormat {
-            form {
-                flash.message = message(code: 'default.created.message', args: [message(code: 'produtoInstance.label', default: 'Produto'), produtoInstance.id])
-                redirect produtoInstance
-            }
-            '*' { respond produtoInstance, [status: CREATED] }
-        }
-    }
+		produtoInstance.save flush:true
 
-    def edit(Produto produtoInstance) {
-        respond produtoInstance
-    }
+		request.withFormat {
+			form {
+				flash.message = message(code: 'default.created.message', args: [
+					message(code: 'produtoInstance.label', default: 'Produto'),
+					produtoInstance.id
+				])
+				redirect produtoInstance
+			}
+			'*' { respond produtoInstance, [status: CREATED] }
+		}
+	}
 
-    @Transactional
-    def update(Produto produtoInstance) {
-        if (produtoInstance == null) {
-            notFound()
-            return
-        }
+	def edit(Produto produtoInstance) {
+		respond produtoInstance
+	}
 
-        if (produtoInstance.hasErrors()) {
-            respond produtoInstance.errors, view:'edit'
-            return
-        }
+	@Transactional
+	def update(Produto produtoInstance) {
+		if (produtoInstance == null) {
+			notFound()
+			return
+		}
 
-        produtoInstance.save flush:true
+		if (produtoInstance.hasErrors()) {
+			respond produtoInstance.errors, view:'edit'
+			return
+		}
 
-        request.withFormat {
-            form {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'Produto.label', default: 'Produto'), produtoInstance.id])
-                redirect produtoInstance
-            }
-            '*'{ respond produtoInstance, [status: OK] }
-        }
-    }
+		produtoInstance.save flush:true
 
-    @Transactional
-    def delete(Produto produtoInstance) {
+		request.withFormat {
+			form {
+				flash.message = message(code: 'default.updated.message', args: [
+					message(code: 'Produto.label', default: 'Produto'),
+					produtoInstance.id
+				])
+				redirect produtoInstance
+			}
+			'*'{ respond produtoInstance, [status: OK] }
+		}
+	}
 
-        if (produtoInstance == null) {
-            notFound()
-            return
-        }
+	@Transactional
+	def delete(Produto produtoInstance) {
 
-        produtoInstance.delete flush:true
+		if (produtoInstance == null) {
+			notFound()
+			return
+		}
 
-        request.withFormat {
-            form {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Produto.label', default: 'Produto'), produtoInstance.id])
-                redirect action:"index", method:"GET"
-            }
-            '*'{ render status: NO_CONTENT }
-        }
-    }
+		produtoInstance.delete flush:true
 
-    protected void notFound() {
-        request.withFormat {
-            form {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'produtoInstance.label', default: 'Produto'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*'{ render status: NOT_FOUND }
-        }
-    }
+		request.withFormat {
+			form {
+				flash.message = message(code: 'default.deleted.message', args: [
+					message(code: 'Produto.label', default: 'Produto'),
+					produtoInstance.id
+				])
+				redirect action:"index", method:"GET"
+			}
+			'*'{ render status: NO_CONTENT }
+		}
+	}
+
+	protected void notFound() {
+		request.withFormat {
+			form {
+				flash.message = message(code: 'default.not.found.message', args: [
+					message(code: 'produtoInstance.label', default: 'Produto'),
+					params.id
+				])
+				redirect action: "index", method: "GET"
+			}
+			'*'{ render status: NOT_FOUND }
+		}
+	}
 }
--- grails-app/controllers/clienteleilao/ServicoController.groovy
+++ grails-app/controllers/clienteleilao/ServicoController.groovy
@@ -12,163 +12,128 @@ import groovy.json.JsonSlurper
 @Transactional(readOnly = true)
 class ServicoController {
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+	static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
-   def index(Integer max) {
-	   def test = new URL("http://luizvarela666-public_sales.nodejitsu.com/services").text
-	   def slurper = new JsonSlurper()
-	   def result = slurper.parseText(test)
-	   
-	   request.setAttribute('result',result)
-	   
-	   respond result
-	   
-       
- }
-	
-   def listaServicos(){
-	   
-	   def test = new URL("http://luizvarela666-public_sales.nodejitsu.com/services").text
-	   
-	   //Retiro o [ inicial e FInal
-	   //test = test.substring(1, test.length() -1)
-   
-	   
-	   //println test
-	   
-	   //Conversao
-	   def slurper = new JsonSlurper()
-	   def result = slurper.parseText(test)
-	   
-   /*
-	   //println result
-	   def emptyMap = []
-	   result.each{
-		   emptyMap.add(it)
-		   println "Nome: ${it.nome}"
-		   println "Descrição: ${it.descricao}"
-		   println "-------------------------------"
-	   }
-	   
-	   println "${emptyMap}"*/
-	   
-	  response result
-	   
-   }
-	
-	
-   def show(Servico servicoInstance) {
-        respond servicoInstance
-    }
+	def index(Integer max) {
+		def test = new URL("http://luizvarela666-public_sales.nodejitsu.com/services").text
+		def slurper = new JsonSlurper()
+		def result = slurper.parseText(test)
+
+		request.setAttribute('result',result)
+
+		respond result
+	}
+
+
+	def show(Servico servicoInstance) {
+		respond servicoInstance
+	}
 
 	def create(){
 		respond new Servico(params)
 	}
 
-    def submitForm() {
+	def submitForm() {
 		def urlBase = "http://luizvarela666-public_sales.nodejitsu.com"
 		def urlPath = "/services"
 		def postData = params
 		withHttp(uri: "http://www.luizvarela666-public_sales.nodejitsu.com/services") {
-		   def retorno = post(body:params)
-		   println retorno
-		   
-		}
-		/*withHttp(uri:"http://luizvarela666-public_sales.nodejitsu.com"){
-			def retorno = post(path:"/services"){
-				headers.'User-Agent' = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.154 Safari/537.36'
-			}
-			println retorno.stat
+			def retorno = post(body:params)
 			println retorno
 		}
-		*/
 		flash.message = "Serviço Cadastrado com Sucesso!"
-		
+
 		redirect(uri: "/")
-		
-		/*def url = new URL("http://luizvarela666-public_sales.nodejitsu.com/services")
-		def http = new HTTPBuilder("http://luizvarela666-public_sales.nodejitsu.com/services")
-		
-		
-        respond new Servico(params)*/
-    }
+	}
 
-    @Transactional
-    def save(Servico servicoInstance) {
-        if (servicoInstance == null) {
-            notFound()
-            return
-        }
+	@Transactional
+	def save(Servico servicoInstance) {
+		if (servicoInstance == null) {
+			notFound()
+			return
+		}
 
-        if (servicoInstance.hasErrors()) {
-            respond servicoInstance.errors, view:'create'
-            return
-        }
+		if (servicoInstance.hasErrors()) {
+			respond servicoInstance.errors, view:'create'
+			return
+		}
 
-        servicoInstance.save flush:true
+		servicoInstance.save flush:true
 
-        request.withFormat {
-            form {
-                flash.message = message(code: 'default.created.message', args: [message(code: 'servicoInstance.label', default: 'Servico'), servicoInstance.id])
-                redirect servicoInstance
-            }
-            '*' { respond servicoInstance, [status: CREATED] }
-        }
-    }
+		request.withFormat {
+			form {
+				flash.message = message(code: 'default.created.message', args: [
+					message(code: 'servicoInstance.label', default: 'Servico'),
+					servicoInstance.id
+				])
+				redirect servicoInstance
+			}
+			'*' { respond servicoInstance, [status: CREATED] }
+		}
+	}
 
-    def edit(Servico servicoInstance) {
-        respond servicoInstance
-    }
+	def edit(Servico servicoInstance) {
+		respond servicoInstance
+	}
 
-    @Transactional
-    def update(Servico servicoInstance) {
-        if (servicoInstance == null) {
-            notFound()
-            return
-        }
+	@Transactional
+	def update(Servico servicoInstance) {
+		if (servicoInstance == null) {
+			notFound()
+			return
+		}
 
-        if (servicoInstance.hasErrors()) {
-            respond servicoInstance.errors, view:'edit'
-            return
-        }
+		if (servicoInstance.hasErrors()) {
+			respond servicoInstance.errors, view:'edit'
+			return
+		}
 
-        servicoInstance.save flush:true
+		servicoInstance.save flush:true
 
-        request.withFormat {
-            form {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'Servico.label', default: 'Servico'), servicoInstance.id])
-                redirect servicoInstance
-            }
-            '*'{ respond servicoInstance, [status: OK] }
-        }
-    }
+		request.withFormat {
+			form {
+				flash.message = message(code: 'default.updated.message', args: [
+					message(code: 'Servico.label', default: 'Servico'),
+					servicoInstance.id
+				])
+				redirect servicoInstance
+			}
+			'*'{ respond servicoInstance, [status: OK] }
+		}
+	}
 
-    @Transactional
-    def delete(Servico servicoInstance) {
+	@Transactional
+	def delete(Servico servicoInstance) {
 
-        if (servicoInstance == null) {
-            notFound()
-            return
-        }
+		if (servicoInstance == null) {
+			notFound()
+			return
+		}
 
-        servicoInstance.delete flush:true
+		servicoInstance.delete flush:true
 
-        request.withFormat {
-            form {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Servico.label', default: 'Servico'), servicoInstance.id])
-                redirect action:"index", method:"GET"
-            }
-            '*'{ render status: NO_CONTENT }
-        }
-    }
+		request.withFormat {
+			form {
+				flash.message = message(code: 'default.deleted.message', args: [
+					message(code: 'Servico.label', default: 'Servico'),
+					servicoInstance.id
+				])
+				redirect action:"index", method:"GET"
+			}
+			'*'{ render status: NO_CONTENT }
+		}
+	}
 
-    protected void notFound() {
-        request.withFormat {
-            form {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'servicoInstance.label', default: 'Servico'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*'{ render status: NOT_FOUND }
-        }
-    }
+	protected void notFound() {
+		request.withFormat {
+			form {
+				flash.message = message(code: 'default.not.found.message', args: [
+					message(code: 'servicoInstance.label', default: 'Servico'),
+					params.id
+				])
+				redirect action: "index", method: "GET"
+			}
+			'*'{ render status: NOT_FOUND }
+		}
+	}
 }
